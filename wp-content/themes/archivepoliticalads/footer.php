9e0a8e5fc1465566ffdd9cd1a90337f70f4868c1
Dan Schultz <slifty@gmail.com>
2016-01-19 15:41:28 -0500

    changed the site name in the footer

--- wp-content/themes/archivepoliticalads/footer.php
+++ wp-content/themes/archivepoliticalads/footer.php
@@ -1,6 +1,6 @@
             <div id="footer" class="row">
                 <div class="footer-section col-xs-12 col-sm-4 col-md-4 ">
-                    <div class="footer-title">PoliticalAd Library</div>
+                    <div class="footer-title">Political TV Ad Archive</div>
                     <div class="footer-subtitle">by the Internet Archive</div>
                     <div class="footer-copy"><?php the_field('home_description', 'option'); ?></div>
                     <div class="footer-link"><a href="http://archive.org/details/tv">archive.org/details/tv</a></div>

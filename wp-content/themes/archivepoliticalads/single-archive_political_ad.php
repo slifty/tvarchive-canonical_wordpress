8937cebda1ec2da37912480f31a5da5687d7ffd4
Dan Schultz <slifty@gmail.com>
2016-01-20 00:18:41 -0500

    vis bug fix

--- wp-content/themes/archivepoliticalads/single-archive_political_ad.php
+++ wp-content/themes/archivepoliticalads/single-archive_political_ad.php
@@ -161,7 +161,6 @@
                                 .start(new Date("<?php echo($ad_first_seen);?>"))
                                 .end((new Date("<?php echo($ad_last_seen);?>")))
                                 .minScale(1)
-                                .hasDelimiter(false)
                                 .eventLineColor(function (datum, index) {
                                     return color(index);
                                 })

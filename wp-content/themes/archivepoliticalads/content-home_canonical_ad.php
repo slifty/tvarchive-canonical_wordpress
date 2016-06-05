7bff93b18b2bc1fd51849c0a9c155aae77f29504
Daniel Letson <daniel.letson@gmail.com>
2016-06-04 17:27:41 -0700

    updated styling and markup for featuredad and blog post

--- wp-content/themes/archivepoliticalads/content-home_canonical_ad.php
+++ wp-content/themes/archivepoliticalads/content-home_canonical_ad.php
@@ -21,18 +21,42 @@
 
         ?>
         <div class="row">
-            <a href="<?php the_permalink(); ?>"><h2 class="ad-title_home">Featured Political Ad</h2></a>
+            <!-- <a href="<?php the_permalink(); ?>"><h2 class="ad-title_home">Featured Political Ad</h2></a> -->
             <div class="col-md-12 hidden-xs hidden-sm">
                 <div class="row">
                     <div id="ad-embed" class="col-md-12">
                         <iframe id="ad-embed-iframe" frameborder="0" width="100%" height="auto" allowfullscreen src="<?php echo($ad_embed_url);?>" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
                     </div>
                 </div>
-                <div id="ad-note_home" class="ad-note_home">
-                    <div class="cell-label cell-label_home">Note</div>
-                    <div class="cell-multiline-value ad-note_content">
-                        <?php echo($ad_notes);?>
-                        <a href="<?php the_permalink(); ?>">READ MORE</a>
+                <div class="row">
+                    <div class="col-md-3 p-l-0 p-r-0">
+                        <div id="ad-sponsor_home" class="ad-info_home">
+                            <div class="cell-label cell-label_home">Sponsor</div>
+                            <div class="cell-value ad-sponsor_content">
+                                <?php foreach ($ad_sponsors as $key => $value) { ?>
+                                    <a class="ad-sponsor_link" href="<?php bloginfo('url'); ?>/browse/?q=<?php echo(urlencode("sponsor:\"".$ad_sponsors[$key]['ad_sponsor']."\""));?>">
+                                        <?php echo ($ad_sponsors[$key]['ad_sponsor']);?></a>
+                                <?php }; ?>
+                            </div>
+                        </div>
+                        <div id="ad-candidate_home" class="ad-info_home">
+                            <div class="cell-label cell-label_home">Candidate</div>
+                            <div class="cell-value ad-candidate_content">
+                                <?php foreach ($ad_candidates as $key => $value) { ?>
+                                    <a class="ad-sponsor_link" href="<?php bloginfo('url'); ?>/browse/?q=<?php echo(urlencode("sponsor:\"".$ad_candidates[$key]['ad_candidate']."\""));?>">
+                                        <?php echo ($ad_candidates[$key]['ad_candidate']);?></a>
+                                <?php }; ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-9 p-l-0 p-r-0">
+                        <div id="ad-note_home" class="ad-note_home">
+                            <div class="cell-label cell-label_home">Note</div>
+                            <div class="cell-multiline-value ad-note_content">
+                                <?php echo substr($ad_notes, 0, 400).'&hellip;';?>
+                                <a href="<?php the_permalink(); ?>">READ MORE</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

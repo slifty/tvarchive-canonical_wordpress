9c788620c0d6a5ed4dda14c32fb74fcb6276ada1
Daniel Letson <daniel.letson@gmail.com>
2016-06-19 20:12:21 -0700

    started explore redesign changes on new feature branch and resolved merge conflicts w/2.0 branch

--- wp-content/themes/archivepoliticalads/front-page.php
+++ wp-content/themes/archivepoliticalads/front-page.php
@@ -37,10 +37,29 @@
             <h1>Explore the Collection</h1>
         </div>
     </div>
-    <div id="home-explore-section">
-        <?php get_template_part('content', 'explore_candidates'); ?>
-        <?php get_template_part('content', 'explore_sponsors'); ?>
-        <?php get_template_part('content', 'explore_sponsor_types'); ?>
+    <div id="home-explore-tabs" class="row explore-tab-row">
+      <div class="col-xs-12 col-md-12">
+        <ul class="nav nav-tabs" id="explore-tabs" role="tablist">
+          <li role="presentation" class="active"><a href="#candidates" aria-controls="candidates" role="tab" data-toggle="tab">By Candidate</a></li>
+          <li role="presentation"><a href="#sponsors" aria-controls="sponsors" role="tab" data-toggle="tab">By Sponsor</a></li>
+          <li role="presentation"><a href="#sponsorTypes" aria-controls="sponsorTypes" role="tab" data-toggle="tab">By Sponsor Type</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="home-explore-section" class="row">
+      <div class="col-xs-12 col-md-12">
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane active" id="candidates">
+            <?php get_template_part('content', 'explore_candidates'); ?>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="sponsors">
+            <?php get_template_part('content', 'explore_sponsors'); ?>
+          </div>
+          <div role="tabpanel" class="tab-pane" id="sponsorTypes">
+            <?php get_template_part('content', 'explore_sponsor_types'); ?>
+          </div>
+        </div>
+      </div>
     </div>
 
     <?php get_footer(); ?>

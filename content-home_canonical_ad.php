b1f8cc60faec16c664b16e06beaa3a73f7153e1c
Dan Schultz <slifty@gmail.com>
2016-09-05 18:27:49 -0400

    Featured ad no longer renders duplicate sponsors
    Some sponsors have the same name but different types.  These are prooperly
    filtered on the canonical page, but weren't being filtered on the home page
    meaning multiple copies of the same type would appear since multiple records
    were associated.

--- content-home_canonical_ad.php
+++ content-home_canonical_ad.php
@@ -6,6 +6,43 @@
         setup_postdata($featured_ad);
         $post_id = get_the_ID();
         $ad = new PoliticalAdArchiveAd($post_id);
+        $ad_embed_url = $post_metadata['embed_url'];
+        $political_ad = new PoliticalAdArchiveAd($post_id);
+        $ad_notes = $political_ad->notes;
+        $transcript = $political_ad->transcript;
+        $archive_id = $political_ad->archive_id;
+        $ad_sponsor_names = $political_ad->sponsor_names;
+        $ad_sponsor_types = $political_ad->sponsor_types;
+        $ad_candidate_names = $political_ad->candidate_names;
+        $ad_subjects = $political_ad->subjects;
+        $ad_type = $political_ad->type;
+        $ad_message = $political_ad->message;
+        $ad_air_count = $political_ad->air_count;
+        $ad_market_count = $political_ad->market_count;
+        $ad_network_count = $political_ad->network_count;
+        $ad_first_seen = $political_ad->first_seen;
+        $ad_last_seen = $political_ad->last_seen;
+
+        // Create sponsor links
+        foreach($ad_sponsor_names as $index => $sponsor_name) {
+            $ad_sponsor_names[$index] = "<a href='".get_bloginfo('url')."/browse/?sponsor_filter=".urlencode($sponsor_name)."'>".$sponsor_name."</a>";
+        }
+
+        // Create sponsor type links
+        foreach($ad_sponsor_types as $index => $sponsor_type) {
+            $ad_sponsor_types[$index] = "<a href='".get_bloginfo('url')."/browse/?sponsor_type_filter=".urlencode($sponsor_type)."''>".$sponsor_type."</a>";
+        }
+        // Create candidate links
+        foreach($ad_candidate_names as $index => $ad_candidate) {
+            $ad_candidate_names[$index] = "<a href='".get_bloginfo('url')."/browse/?candidate_filter=".urlencode($ad_candidate)."''>".$ad_candidate."</a>";
+        }
+
+        // Create subject links
+        foreach($ad_subjects as $index => $ad_subject) {
+            $ad_subjects[$index] = "<a href='".get_bloginfo('url')."/browse/?subject_filter=".urlencode($ad_subject)."''>".$ad_subject."</a>";
+        }
+        ?>
+
         ?>
         <div class="row">
             <div class="col-md-12">
@@ -19,21 +56,13 @@
                         <div id="ad-sponsor_home" class="ad-info_home col-sm-6 col-md-12">
                             <div class="cell-label cell-label_home">Sponsor</div>
                             <div class="cell-value ad-sponsor_content">
-                                <?php foreach ($ad->sponsors as $sponsor) { ?>
-                                    <a class="ad-sponsor_link" href="<?php bloginfo('url'); ?>/browse?sponsor_filter=<?php echo(urlencode($sponsor->name));?>">
-                                        <?php echo ($sponsor->name);?>
-                                    </a>
-                                <?php }; ?>
+                                <?php echo(implode(', ', $ad_sponsor_names)); ?>
                             </div>
                         </div>
                         <div id="ad-candidate_home" class="ad-info_home col-sm-6 col-md-12">
                             <div class="cell-label cell-label_home">Candidate</div>
                             <div class="cell-value ad-candidate_content">
-                                <?php foreach ($ad->candidates as $candidate) { ?>
-                                    <a class="ad-sponsor_link" href="<?php bloginfo('url'); ?>/browse?candidate_filter=<?php echo(urlencode($candidate->name));?>">
-                                        <?php echo($candidate->name);?> (<?php echo($candidate->affiliation);?>)
-                                    </a>
-                                <?php }; ?>
+                                <?php echo(implode(', ', $ad_candidate_names)); ?>
                             </div>
                         </div>
                     </div>

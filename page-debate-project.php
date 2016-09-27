f9fb34ee378f7f8e0d03a65639c638f0b9147235
Dan Schultz <slifty@gmail.com>
2016-09-27 15:01:25 -0400

    Added links to the raw data file

--- page-debate-project.php
+++ page-debate-project.php
@@ -21,17 +21,6 @@
         </div>
     </div>
 
-
-    <div class="row debate-header guttered-row">
-        <div class="col-xs-12 col-md-12">
-            <h2 class="section-header">Watch the Debates</h2>
-        </div>
-    </div>
-
-    <div id="debate-embed" class="row">
-        <iframe id="ad-embed-iframe" class="col-xs-12" frameborder="0" allowfullscreen src="<?php echo($ad_embed_url);?>&nolinks=1" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>
-    </div>
-
     <div class="row debate-header guttered-row">
         <div class="col-xs-12">
             <h2 class="section-header">Download the Data</h2>
@@ -50,15 +39,15 @@
                 <h2 class="debate-title">First Debate: September 26, 2016</h2>
                 <div class="row">
                     <div class="data-download col-xs-12 col-sm-4">
-                        <a href="" class="btn primary data-download__button" target="_blank">Raw Matches</a>
+                        <a href="<?php echo(get_stylesheet_directory_uri());?>/data/20160927_2_30_raw.csv" class="btn primary data-download__button" target="_blank">Raw Matches</a>
                         <small class="data-download__size">CSV</small>
                     </div>
-                    <div class="data-download col-xs-12 col-sm-4">
-                        <a href="" class="btn primary data-download__button" target="_blank">Frequency Timeline</a>
+                    <div class="<?php echo(get_stylesheet_directory_uri());?>/data-download col-xs-12 col-sm-4">
+                        <a href="data/20160927_2_30_timeline.csv" class="btn primary data-download__button" target="_blank">Frequency Timeline</a>
                         <small class="data-download__size">CSV</small>
                     </div>
-                    <div class="data-download col-xs-12 col-sm-4">
-                        <a href="" class="btn primary data-download__button" target="_blank">Summarized Matches</a>
+                    <div class="<?php echo(get_stylesheet_directory_uri());?>/data-download col-xs-12 col-sm-4">
+                        <a href="data/20160927_2_30_summarized.csv" class="btn primary data-download__button" target="_blank">Summarized Matches</a>
                         <small class="data-download__size">CSV</small>
                     </div>
                 </div>

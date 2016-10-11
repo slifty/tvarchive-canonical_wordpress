bda87658590a7732ed68f7d518dabab8357a3701
Dan Schultz <slifty@gmail.com>
2016-10-11 17:57:05 -0400

    24h second debate data

--- page-debate-project.php
+++ page-debate-project.php
@@ -156,6 +156,31 @@
         </div>
     </div>
 
+    <div class="row guttered-row" style="padding-bottom: 20px;">
+        <div class="col-xs-12"></div>
+    </div>
+    <div class="row guttered-row">
+        <div class="col-xs-12">
+            <div class="data-download-group-container">
+                <h2 class="debate-title">Second Presidential Debate (Cable stations): October 9, 2016</h2>
+                <p class="debate-data-description">Data on debate clips aired during the 26 hours following the debate, October 9 – 10, 2016.  Including CNN, MSNBC, Fox News and more</p>
+                <div class="row">
+                    <div class="data-download col-xs-12 col-sm-4">
+                        <a href="<?php echo(get_stylesheet_directory_uri());?>/data/2016_10_09_second_presidential_full_cable_1476215812_raw.csv" class="btn primary data-download__button" target="_blank">Raw Matches</a>
+                        <small class="data-download__size">CSV</small>
+                    </div>
+                    <div class="data-download col-xs-12 col-sm-4">
+                        <a href="<?php echo(get_stylesheet_directory_uri());?>/data/2016_10_09_second_presidential_full_cable_1476215812_timeline.csv" class="btn primary data-download__button" target="_blank">Frequency Timeline</a>
+                        <small class="data-download__size">CSV</small>
+                    </div>
+                    <div class="data-download col-xs-12 col-sm-4">
+                        <a href="<?php echo(get_stylesheet_directory_uri());?>/data/2016_10_09_second_presidential_full_cable_1476215812_summarized.csv" class="btn primary data-download__button" target="_blank">Summarized Matches</a>
+                        <small class="data-download__size">CSV</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row guttered-row" style="padding-bottom: 20px;">
         <div class="col-xs-12"></div>
@@ -163,7 +188,7 @@
     <div class="row guttered-row">
         <div class="col-xs-12">
             <div class="data-download-group-container">
-                <h2 class="debate-title">Second Presidential Debate Debate (Local stations): October 9, 2016</h2>
+                <h2 class="debate-title">Second Presidential Debate (Local stations): October 9, 2016</h2>
                 <p class="debate-data-description"><strong>Morning:</strong> Clips aired in early morning local news programs, on ABC, CBS, NBC, and Fox affiliates–in select battleground state markets from 5:00 am to 7:00 am on October 10, 2016.</p>
                 <p class="debate-data-description"><strong>Full:</strong> Clips aired over the 26 hours following the debate by local stations in select battleground state markets.</p>
                 <div class="row">
@@ -172,7 +197,7 @@
                         <small class="data-download__size">CSV</small>
                     </div>
                     <div class="data-download col-xs-12 col-sm-4">
-                        <a href="" class="btn primary data-download__button" target="_blank">Full (coming soon)</a>
+                        <a href="<?php echo(get_stylesheet_directory_uri());?>/data/2016_10_09_second_presidential_full_local_1476216430_raw.csv" class="btn primary data-download__button" target="_blank">Full (raw)</a>
                         <small class="data-download__size">CSV</small>
                     </div>
                 </div>
@@ -183,10 +208,19 @@
 
 
 
+    <div class="row guttered-row" style="padding-bottom: 20px;">
+        <div class="col-xs-12"></div>
+    </div>
+
     <div class="row debate-header guttered-row">
         <div class="col-xs-12">
             <h2 class="section-header">Special Datasets</h2>
         </div>
+    </div>
+
+
+    <div class="row guttered-row" style="padding-bottom: 20px;">
+        <div class="col-xs-12"></div>
     </div>
 
     <div class="row guttered-row">
@@ -212,6 +246,11 @@
             </div>
         </div>
     </div>
+
+    <div class="row guttered-row" style="padding-bottom: 20px;">
+        <div class="col-xs-12"></div>
+    </div>
+
     <div class="row guttered-row">
         <div class="col-xs-12">
             <div class="data-download-group-container">

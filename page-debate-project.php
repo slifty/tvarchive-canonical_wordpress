aaef7d20559d2c6b47eda64a23dd6e66e7e90832
Dan Schultz <slifty@gmail.com>
2016-10-20 15:07:10 -0400

    copypasta typo

--- page-debate-project.php
+++ page-debate-project.php
@@ -285,8 +285,8 @@
     <div class="row guttered-row">
         <div class="col-xs-12">
             <div class="data-download-group-container">
-                <h2 class="debate-title">Second Presidential Debate (Local stations): October 9, 2016</h2>
-                <p class="debate-data-description"><strong>Morning:</strong> Clips aired in early morning local news programs, on ABC, CBS, NBC, and Fox affiliates–in select battleground state markets from 5:00 am to 7:00 am on October 10, 2016.</p>
+                <h2 class="debate-title">Third Presidential Debate (Local stations): October 19, 2016</h2>
+                <p class="debate-data-description"><strong>Morning:</strong> Clips aired in early morning local news programs, on ABC, CBS, NBC, and Fox affiliates–in select battleground state markets from 5:00 am to 7:00 am on October 20, 2016.</p>
                 <p class="debate-data-description"><strong>Full:</strong> Clips aired over the 26 hours following the debate by local stations in select battleground state markets.</p>
                 <div class="row">
                     <div class="data-download col-xs-12 col-sm-4">
